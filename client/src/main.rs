855fc6eb726bdb67ff5502296672701ed1c05d76
MegaBluejay <dmoiseev2011@gmail.com>
2023-10-18 02:21:04 +0300

    Add number of requests as argument

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,19 +9,20 @@ use stilsoft_common::call_timing::CallTimedService;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let nreqs: u32 = std::env::args()
+        .nth(1)
+        .ok_or_else(|| anyhow!("nreqs not given"))?
+        .parse()?;
+
     let mut client = CallTimedService::new(Timeout::new(
         Client::builder().http2_only(true).build_http::<Body>(),
         Duration::from_secs(2),
     ));
 
-    let mut futs: FuturesUnordered<_> = (1..10).map(|i| client.call(mk_req(i))).collect();
+    let mut futs: FuturesUnordered<_> = (1..=nreqs).map(|i| client.call(mk_req(i))).collect();
 
     while let Some(res) = futs.try_next().await.map_err(|e| anyhow!(e))? {
-        println!(
-            "{}: {}",
-            std::env::args().collect::<Vec<_>>()[1],
-            to_bytes(res.into_body()).await?.escape_ascii()
-        );
+        println!("{}", to_bytes(res.into_body()).await?.escape_ascii());
     }
 
     Ok(())
